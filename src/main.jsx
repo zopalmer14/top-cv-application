e34b67f0a817de856ad78f96df3efbe6bab4edbc
zopalmer14 <zopalmer14@gmail.com>
2024-05-05 18:23:31 -0400

    add content to other cv sections

--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,26 @@ import ReactDOM from 'react-dom/client'
 import Section from './components/Section.jsx'
 import './index.css'
 
-const inputList = [
+const personalInputs = [
   {
     type: 'text',
     label: 'Full Name',
     name: 'name',
-    placeholder: 'First and last name',
+    placeholder: 'First and Last Name',
     optText: ''
   },
   {
     type: 'email',
     label: 'Email',
     name: 'email',
-    placeholder: 'Enter email',
+    placeholder: 'Enter Email',
     optText: 'recommended'
   },
   {
     type: 'tel',
-    label: 'Phone number',
+    label: 'Phone Number',
     name: 'phone',
-    placeholder: 'Enter phone number',
+    placeholder: 'Enter Phone Number',
     optText: 'recommended'
   },
   {
@@ -34,10 +34,93 @@ const inputList = [
   }
 ]
 
+const educationInputs = [
+  {
+    type: 'text',
+    label: 'School',
+    name: 'school',
+    placeholder: 'Enter School / University',
+    optText: ''
+  },
+  {
+    type: 'text',
+    label: 'Degree',
+    name: 'degree',
+    placeholder: 'Enter Degree / Field of Study',
+    optText: ''
+  },
+  {
+    type: 'date',
+    label: 'Start Date',
+    name: 'startDate',
+    placeholder: 'Enter Start Date',
+    optText: ''
+  },
+  {
+    type: 'date',
+    label: 'End Date',
+    name: 'endDate',
+    placeholder: 'Enter End Date',
+    optText: ''
+  },
+  {
+    type: 'text',
+    label: 'Location',
+    name: 'location',
+    placeholder: 'City, Country',
+    optText: 'optional'
+  }
+]
+
+const experienceInputs = [
+  {
+    type: 'text',
+    label: 'Company Name',
+    name: 'company',
+    placeholder: 'Enter Company Name',
+    optText: ''
+  },
+  {
+    type: 'text',
+    label: 'Position Title',
+    name: 'position',
+    placeholder: 'Enter Position Title',
+    optText: ''
+  },
+  {
+    type: 'date',
+    label: 'Start Date',
+    name: 'startDate',
+    placeholder: 'Enter Start Date',
+    optText: ''
+  },
+  {
+    type: 'date',
+    label: 'End Date',
+    name: 'endDate',
+    placeholder: 'Enter End Date',
+    optText: ''
+  },
+  {
+    type: 'text',
+    label: 'Location',
+    name: 'location',
+    placeholder: 'City, Country',
+    optText: 'optional'
+  },
+  {
+    type: 'textarea',
+    label: 'Description',
+    name: 'desc',
+    placeholder: 'Enter Description',
+    optText: 'optional'
+  }
+]
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Section title="Personal Details" inputs={inputList} />
-    <Section title="Education" />
-    <Section title="Work Experience" />
+    <Section title="Personal Details" inputs={personalInputs} />
+    <Section title="Education" inputs={educationInputs} />
+    <Section title="Work Experience" inputs={experienceInputs} />
   </React.StrictMode>,
 )
